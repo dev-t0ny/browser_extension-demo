4cbd00d7360d51749c61f807e8db04caacc48fc7
dev-tony <antoineboudreau@outlook.com>
2024-04-26 13:57:11 -0400

    Fixed bug: page tracker

--- content.js
+++ content.js
@@ -1,268 +1,262 @@
 'use strict';
 /* global browser */
-
-const messyBr = document.getElementsByTagName('br');
-const brArray = Array.from(messyBr);  // Convert the live HTMLCollection to a static array
-const rightSec = document.getElementsByClassName('section-droite');
-const leftSec = document.getElementsByClassName('parent-menu-gauche');
-
-for (let brTag of brArray) {
-    if (!brTag.parentElement.classList.contains('right-section')) {
-        brTag.remove();
-    }
+let secCentre;
+let leaLanding = false;
+//checks if this is lea's landing page
+if (document.getElementsByClassName('section-centre')[0]) {
+    secCentre = document.getElementsByClassName('section-centre')[0];
+    leaLanding = true;
+}
+if (leaLanding) {
+    leaLandingHandler(secCentre);
 }
 
-let classListDOM = document.getElementsByClassName('section-spacing');
-let classListUser = [];
+function leaLandingHandler(secCentre) {
+    const messyBr = document.getElementsByTagName('br');
+    const brArray = Array.from(messyBr);  // Convert the live HTMLCollection to a static array
+    const rightSec = document.getElementsByClassName('section-droite');
+    const leftSec = document.getElementsByClassName('parent-menu-gauche');
 
-let canvaElement = document.createElement('canvas');
-canvaElement.id = 'myChart';
-canvaElement.width = '1200';
-canvaElement.height = '400';
-canvaElement.style = 'display: inline;';
-let secCentre = document.getElementsByClassName('section-centre')[0];
-let cards = document.getElementsByClassName('classes-wrapper')[0];
-secCentre.style.alignItems = 'start';
-cards.style.marginLeft = '0';
-cards.style.marginRight = 'auto';
-secCentre.appendChild(canvaElement);
-
-if (classListDOM.length != 0) {
-    for (let i = 0; i < classListDOM.length; i++) {
-        classListUser.push(
-            {
-                name: classListDOM[i].getElementsByClassName('card-panel-title')[0].textContent,
-                grade: classListDOM[i].getElementsByClassName('pourcentage')[0].textContent,
-                average: classListDOM[i].getElementsByClassName('note-principale')[1].textContent
-            });
-    }
-    // Get the canvas element and its context
-    const canvas = document.getElementById('myChart');
-    const ctx = canvas.getContext('2d');
-
-    const data = classListUser;
-    // Calculate the maximum grade to determine chart scaling
-    const maxValue = Math.max(...data.map(item => parseInt(item.grade))) + 10;
-
-    // Settings for the chart
-    const chartWidth = 1200;
-    const barWidth = 50;
-    const barSpacing = 80;
-    const chartHeight = 400;
-    const scaleFactor = (chartHeight - 50) / maxValue;
-    const baseHeight = chartHeight - 30;
-
-    // Draw bars
-    data.forEach((item, index) => {
-
-        let currentGrade = parseInt(item.grade.substring(0, 2));
-        if (currentGrade > 60) {
-            ctx.fillStyle = '#4A90E2';
+    for (let brTag of brArray) {
+        if (!brTag.parentElement.classList.contains('right-section')) {
+            brTag.remove();
         }
-        else if (currentGrade > 45) {
-            ctx.fillStyle = '#FFBF00';
+    }
+
+    let classListDOM = document.getElementsByClassName('section-spacing');
+    let classListUser = [];
+
+    let canvaElement = document.createElement('canvas');
+    canvaElement.id = 'myChart';
+    canvaElement.width = '1200';
+    canvaElement.height = '400';
+    canvaElement.style = 'display: inline;';
+
+    let cards = document.getElementsByClassName('classes-wrapper')[0];
+    secCentre.style.alignItems = 'start';
+    cards.style.marginLeft = '0';
+    cards.style.marginRight = 'auto';
+    secCentre.appendChild(canvaElement);
+
+    if (classListDOM.length != 0) {
+        for (let i = 0; i < classListDOM.length; i++) {
+            classListUser.push(
+                {
+                    name: classListDOM[i].getElementsByClassName('card-panel-title')[0].textContent,
+                    grade: classListDOM[i].getElementsByClassName('pourcentage')[0].textContent,
+                    average: classListDOM[i].getElementsByClassName('note-principale')[1].textContent
+                });
+        }
+        // Get the canvas element and its context
+        const canvas = document.getElementById('myChart');
+        const ctx = canvas.getContext('2d');
+
+        const data = classListUser;
+        // Calculate the maximum grade to determine chart scaling
+        const maxValue = Math.max(...data.map(item => parseInt(item.grade))) + 10;
+
+        // Settings for the chart
+        const chartWidth = 1200;
+        const barWidth = 50;
+        const barSpacing = 80;
+        const chartHeight = 400;
+        const scaleFactor = (chartHeight - 50) / maxValue;
+        const baseHeight = chartHeight - 30;
+
+        // Draw bars
+        data.forEach((item, index) => {
+
+            let currentGrade = parseInt(item.grade.substring(0, 2));
+            if (currentGrade > 60) {
+                ctx.fillStyle = '#4A90E2';
+            }
+            else if (currentGrade > 45) {
+                ctx.fillStyle = '#FFBF00';
+            }
+            else {
+                ctx.fillStyle = '#C70039';
+            }
+
+            const barHeight = parseInt(item.grade) * scaleFactor;
+            const x = 60 + index * (barWidth + barSpacing);
+            const y = baseHeight - barHeight;
+
+            ctx.fillRect(x, y, barWidth, barHeight);
+
+            // Add labels over each bar
+            ctx.fillStyle = '#000';
+            ctx.textAlign = 'center';
+            ctx.fillText(item.grade, x + barWidth / 2, baseHeight - 50);
+
+            // Add labels below each bar
+            ctx.fillStyle = '#000';
+            ctx.textAlign = 'center';
+            ctx.fillText(item.name.substring(10, 15), x + barWidth, baseHeight + 20);
+
+            const currentAverage = item.average.substring(0, 2);
+
+            //if there's an average, add a column
+            if (!currentAverage.includes('-')) {
+                ctx.fillStyle = '#808080';
+                const averageBarHeight = parseInt(currentAverage) * scaleFactor;
+                const averageX = x + barWidth + 10;
+                ctx.fillRect(averageX, baseHeight - averageBarHeight, barWidth - 20, averageBarHeight);
+                ctx.fillStyle = '#000';
+                ctx.fillText(currentAverage + '%', averageX + 15, baseHeight - 50);
+            }
+        });
+        ctx.beginPath();
+        ctx.moveTo(40, 30);
+        ctx.lineTo(40, baseHeight);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(40, baseHeight);
+        ctx.lineTo(chartWidth - 100, baseHeight);
+        ctx.stroke();
+    }
+    secCentre.style.flexDirection = 'column';
+    /**
+     * Sets canva's visibility
+     * @param {*} e promis
+     */
+    function setVisibility(e) {
+        console.log(e);
+        if (e.chkState) {
+            canvaElement.style.display = 'inline';
+
         }
         else {
-            ctx.fillStyle = '#C70039';
+            canvaElement.style.display = 'none';
+
         }
-
-        const barHeight = parseInt(item.grade) * scaleFactor;
-        const x = 60 + index * (barWidth + barSpacing);
-        const y = baseHeight - barHeight;
-
-        ctx.fillRect(x, y, barWidth, barHeight);
-
-        // Add labels over each bar
-        ctx.fillStyle = '#000';
-        ctx.textAlign = 'center';
-        ctx.fillText(item.grade, x + barWidth / 2, baseHeight - 50);
-
-        // Add labels below each bar
-        ctx.fillStyle = '#000';
-        ctx.textAlign = 'center';
-        ctx.fillText(item.name.substring(10, 15), x + barWidth, baseHeight + 20);
-
-        const currentAverage = item.average.substring(0, 2);
-
-        //if there's an average, add a column
-        if (!currentAverage.includes('-')) {
-            ctx.fillStyle = '#808080';
-            const averageBarHeight = parseInt(currentAverage) * scaleFactor;
-            const averageX = x + barWidth + 10;
-            ctx.fillRect(averageX, baseHeight - averageBarHeight, barWidth - 20, averageBarHeight);
-            ctx.fillStyle = '#000';
-            ctx.fillText(currentAverage + '%', averageX + 15, baseHeight - 50);
-        }
-    });
-    ctx.beginPath();
-    ctx.moveTo(40, 30);
-    ctx.lineTo(40, baseHeight);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(40, baseHeight);
-    ctx.lineTo(chartWidth - 100, baseHeight);
-    ctx.stroke();
-}
-secCentre.style.flexDirection = 'column';
-/**
- * Sets canva's visibility
- * @param {*} e promis
- */
-function setVisibility(e) {
-    console.log(e);
-    if (e.chkState) {
-        canvaElement.style.display = 'inline';
-
     }
-    else {
-        canvaElement.style.display = 'none';
- 
-    }
-}
-/**
- * Sets side sections visibility
- * @param {*} e event
- */
-function setSideSections(e) {
-    //user preference is true (VISIBLE)
-    if (e.chkUseless) {
-        for (let element of rightSec)
-        {
-            element.style.display = 'inline';
-        }
-        for (let element of leftSec)
-        {
-            element.style.display = 'inline';
-        }
-        
-        let secCentre = document.getElementsByClassName('section-centre')[0];
+    /**
+     * Sets side sections visibility
+     * @param {*} e event
+     */
+    function setSideSections(e) {
+        //user preference is true (VISIBLE)
+        if (e.chkUseless) {
+            for (let element of rightSec) {
+                element.style.display = 'inline';
+            }
+            for (let element of leftSec) {
+                element.style.display = 'inline';
+            }
 
-        secCentre.style.width = '50rem';
-        cards.style.display = 'inline';
-        cards.style.gap = '0';
-        document.querySelector('.classes-titre').style.display = 'inline';
-        document.querySelector('.id-service_CVIP').style.display = 'inline-block';
-    }
-    //user preference is false (HIDDEN)
-    else {
-        for (let element of rightSec)
-        {
-            element.style.display = 'none';
-        }
-        for (let element of leftSec)
-        {
-            element.style.display = 'none';
-        }
+            let secCentre = document.getElementsByClassName('section-centre')[0];
 
-        let secCentre = document.getElementsByClassName('section-centre')[0];
-        secCentre.style.width = 'auto';
-        cards.style.display = 'grid';
-        let gridTemplate;
-        if (window.innerWidth > 1454)
-        {
-            gridTemplate = 'auto auto auto auto';
-        }
-
-        else if (window.innerWidth > 1166)
-        {
-            gridTemplate = 'auto auto auto';
-        }
-        else
-        {
-            gridTemplate = 'auto auto';
-        }
-
-
-        cards.style.gridTemplateColumns = gridTemplate;
-        cards.style.gap = '1.5rem';
-        document.querySelector('.classes-titre').style.display = 'none';
-        document.querySelector('.id-service_CVIP').style.display = 'none';
-    }
-}
-
-let prendreState = browser.storage.sync.get('chkState');
-prendreState.then(setVisibility, (e) => { console.log(e);});
-
-prendreState = browser.storage.sync.get('chkUseless');
-prendreState.then(setSideSections, (e) => { console.log(e);});
-
-
-// Listen for messages from the popup script
-browser.runtime.onMessage.addListener((message) => {
-
-    //user has checked/unchecked the chart chkbox
-    if (message.command === 'toggle-chart') {
-        // Toggle visibility based on current state
-        canvaElement.style.display = (canvaElement.style.display === 'inline') ? 'none' : 'inline';
-
-    }
-
-    //User has checked/unchecked the sections cleaner chkbox
-    else if (message.command === 'toggle-useless') {
-        let secCentre = document.getElementsByClassName('section-centre')[0];
-
-        //styling
-        for (let element of rightSec)
-        {
-            element.style.display = (element.style.display === 'inline') ? 'none' : 'inline';
-        }
-
-        for (let element of leftSec)
-        {
-            element.style.display = (element.style.display === 'inline') ? 'none' : 'inline';
-        }
-
-        //chkbox is CHECKED (sections are still visible)
-        if (rightSec[0].style.display === 'inline')
-        {
-
-            document.querySelector('.id-service_CVIP').style.display = 'inline-block';
-
-            cards.style.marginLeft = '2rem';
-            cards.style.marginRight = '0';
             secCentre.style.width = '50rem';
             cards.style.display = 'inline';
             cards.style.gap = '0';
             document.querySelector('.classes-titre').style.display = 'inline';
+            document.querySelector('.id-service_CVIP').style.display = 'inline-block';
         }
+        //user preference is false (HIDDEN)
+        else {
+            for (let element of rightSec) {
+                element.style.display = 'none';
+            }
+            for (let element of leftSec) {
+                element.style.display = 'none';
+            }
 
-        //chkbox is UNCHECKED (sections are not visible)
-        else
-        {
-            //removes the lea button, which is useless on this page
-            document.querySelector('.id-service_CVIP').style.display = 'none';
-
-            cards.style.marginLeft = '0';
-            cards.style.marginRight = 'auto';
+            let secCentre = document.getElementsByClassName('section-centre')[0];
             secCentre.style.width = 'auto';
             cards.style.display = 'grid';
             let gridTemplate;
-
-            //responsive design
-            if (window.innerWidth > 1454)
-            {
+            if (window.innerWidth > 1454) {
                 gridTemplate = 'auto auto auto auto';
             }
-    
-            else if (window.innerWidth > 1166)
-            {
+
+            else if (window.innerWidth > 1166) {
                 gridTemplate = 'auto auto auto';
             }
-            else
-            {
+            else {
                 gridTemplate = 'auto auto';
             }
-    
-    
+
+
             cards.style.gridTemplateColumns = gridTemplate;
             cards.style.gap = '1.5rem';
             document.querySelector('.classes-titre').style.display = 'none';
+            document.querySelector('.id-service_CVIP').style.display = 'none';
+        }
+    }
+
+    let prendreState = browser.storage.sync.get('chkState');
+    prendreState.then(setVisibility, (e) => { console.log(e); });
+
+    prendreState = browser.storage.sync.get('chkUseless');
+    prendreState.then(setSideSections, (e) => { console.log(e); });
+
+
+    // Listen for messages from the popup script
+    browser.runtime.onMessage.addListener((message) => {
+
+        //user has checked/unchecked the chart chkbox
+        if (message.command === 'toggle-chart') {
+            // Toggle visibility based on current state
+            canvaElement.style.display = (canvaElement.style.display === 'inline') ? 'none' : 'inline';
+
         }
 
-    }
-});
+        //User has checked/unchecked the sections cleaner chkbox
+        else if (message.command === 'toggle-useless') {
+            let secCentre = document.getElementsByClassName('section-centre')[0];
+
+            //styling
+            for (let element of rightSec) {
+                element.style.display = (element.style.display === 'inline') ? 'none' : 'inline';
+            }
+
+            for (let element of leftSec) {
+                element.style.display = (element.style.display === 'inline') ? 'none' : 'inline';
+            }
+
+            //chkbox is CHECKED (sections are still visible)
+            if (rightSec[0].style.display === 'inline') {
+
+                document.querySelector('.id-service_CVIP').style.display = 'inline-block';
+
+                cards.style.marginLeft = '2rem';
+                cards.style.marginRight = '0';
+                secCentre.style.width = '50rem';
+                cards.style.display = 'inline';
+                cards.style.gap = '0';
+                document.querySelector('.classes-titre').style.display = 'inline';
+            }
+
+            //chkbox is UNCHECKED (sections are not visible)
+            else {
+                //removes the lea button, which is useless on this page
+                document.querySelector('.id-service_CVIP').style.display = 'none';
+
+                cards.style.marginLeft = '0';
+                cards.style.marginRight = 'auto';
+                secCentre.style.width = 'auto';
+                cards.style.display = 'grid';
+                let gridTemplate;
+
+                //responsive design
+                if (window.innerWidth > 1454) {
+                    gridTemplate = 'auto auto auto auto';
+                }
+
+                else if (window.innerWidth > 1166) {
+                    gridTemplate = 'auto auto auto';
+                }
+                else {
+                    gridTemplate = 'auto auto';
+                }
 
 
+                cards.style.gridTemplateColumns = gridTemplate;
+                cards.style.gap = '1.5rem';
+                document.querySelector('.classes-titre').style.display = 'none';
+            }
 
-
+        }
+    });
+}
